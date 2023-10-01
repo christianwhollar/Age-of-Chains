da030aa59cc203d36d1d9173a7aaed721df0fb77
Christian Hollar <hollarc@lafayette.edu>
2023-10-01 17:18:14 -0400

    Initial attempt at Search Engine tool.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from agents.dispatcher import Dispatcher
+import logging
+from tools.logging_config import configure_logging
+from tools.search_engines.google_search import GoogleSearch
+from tools.search_engines.bing_search import BingSearch
+from tools.web_utils.web_search import SearchEngine, WebSearch
+from rich.console import Console
 
-if __name__ == '__main__':
-    d = Dispatcher()
-    response = d.generate_response('Hello there!')
-    print(response)
+configure_logging()
+logger = logging.getLogger(__name__)
+console = Console()
+
+if __name__ == "__main__":
+    # Search Engine
+    google_engine = SearchEngine(GoogleSearch)
+    bing_engine = SearchEngine(BingSearch)
+
+    # Web Search
+    query = 'Python'
+    web_search = WebSearch(google_engine, query = query)
+    links = web_search.search()
+    web_search.print_links(links)  
+    web_page = web_search.open(links[0])
+    # print(web_page.title)
+    # print(web_page.body)
---
+++ tools/README.md
@@ -0,0 +1,16 @@
+tools/
+    ├── search_engines/
+    │    ├── __init__.py
+    │    ├── search_engine_base.py
+    │    ├── google_search.py
+    │    └── bing_search.py
+    ├── web_utils/
+    │    ├── __init__.py
+    │    ├── web_link.py
+    │    ├── web_page.py
+    │    ├── web_search.py
+    │    └── exceptions.py
+    ├── logging_config.py
+    ├── requirements.txt
+    ├── main.py
+    └── DIRECTORY_STRUCTURE.txt or README.md
